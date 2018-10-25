45e3f63f81128abab5e40020f1e87c8f7ccb4318
C3NZ <cenz@cenz.io>
2018-10-25 13:20:34 -0700

    Reverse a string given as an argument to our program

---
+++ reverse.py
@@ -0,0 +1,17 @@
+import sys
+
+def reverse(string):
+    '''
+        Reverses a string
+        Assumes string is a string object to be reversed
+    '''
+    return string[::-1]
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Incorrect usage of the program")
+        print("Example usage: python3 reverse.py hello")
+        print("output: olleh")
+    else:
+        print(reverse(sys.argv[1]))
+
