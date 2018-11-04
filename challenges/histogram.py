90c2fad40940d5b8c7c5ee24365653e74d30d70a
C3NZ <cenz@cenz.io>
2018-11-03 20:17:28 -0700

    Pushing to desktop

--- challenges/histogram.py
+++ challenges/histogram.py
@@ -41,7 +41,7 @@ def unique_words(histogram):
         Finds all the unique words inside of a dictionary histogram
         Assumes histogram is a dictionary
     '''
-    return histogram.keys()
+    return len(histogram.keys())
 
 
 def frequency(histogram, word):
@@ -153,6 +153,7 @@ def histogram_tup(source_text, is_file=True):
         words = source_text.split()
         for word in words:
             process_text_tup(histogram, word)
+    return histogram
 
 def unique_words_tup(histogram):
     '''
@@ -172,6 +173,80 @@ def frequency_tup(histogram, word):
             return item[1]
     return 0
 
+def process_text_count(histogram, word):
+    word = word.lower();
+    if len(histogram) == 0:
+        histogram.append((1, [word]))
+    else:
+        count = 0
+        word_found = False
+
+        for item in histogram:
+            selected_words = item[1]
+
+            if word_found and count == item[0]:
+                selected_words.append(word)
+                return
+            else:
+                if word in selected_words:
+                    count = item[0] + 1
+                    word_found = True
+                    selected_words.remove(word)
+
+        if not word_found:
+            histogram[0][1].append(word)
+        else:
+            histogram.append((count, [word]))
+
+def remove_empties(histogram):
+    for i in range(0, len(histogram) - 1):
+        current_item = histogram[i]
+
+        if not len(current_item[1]):
+            histogram.pop(i)
+
+def histogram_count(source_text, is_file=True):
+    '''
+        Create a count based histogram, where items are stored into tuples based on count
+    '''
+    histogram = []
+
+    if is_file:
+        file = open(source_text, 'r')
+        lines = file.readlines()
+        for line in lines.split():
+            for word in line:
+                process_text_count(histogram, word)
+    else:
+        words = source_text.split()
+        for word in words:
+            process_text_count(histogram, word)
+
+    remove_empties(histogram)
+
+    return histogram
+
+def unique_words_count(histogram):
+    unique_count = 0
+
+    for item in histogram:
+        unique_count += len(item[1])
+
+    return unique_count
+
+def frequency_count(histogram, word):
+    frequency = 0
+    word = word.lower()
+
+    for item in histogram:
+        word_list = item[1]
+        for current_word in word_list:
+            if word == current_word:
+                frequency = item[0]
+                return frequency
+
+    return frequency
+
 if __name__ == '__main__':
     histogram = histogram('Hi there Hi bye hi hello bye bye bye hello', is_file=False)
     print(histogram)
@@ -187,3 +262,8 @@ if __name__ == '__main__':
     print(histogram_tup)
     print(unique_words_tup(histogram_tup))
     print(frequency_tup(histogram_tup, 'HI'))
+
+    histogram_count = histogram_count("hi there there hi bye hi hello bye bye bye hello", is_file=False)
+    print(histogram_count)
+    print(unique_words_count(histogram_count))
+    print(frequency_count(histogram_count, 'HI'))
