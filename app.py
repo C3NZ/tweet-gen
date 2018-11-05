67cbb60dbc2cd0855bf5491e22f14a73818c2f5f
C3NZ <cenz@cenz.io>
2018-11-04 16:49:29 -0800

    [fix] allowed application to be binded to ports specified as env variables

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
+'''
+    Application for hosting our histogram
+'''
+import os
+
 from flask import Flask
 
-from .challenges.histograms.distogram import histogram
-from .challenges.histograms import sample
+from challenges.histograms.distogram import histogram
+from challenges.histograms import sample
 
 app = Flask(__name__)
 
 def create_histogram():
+    '''
+    Create the histogram
+    '''
     return histogram('moby.txt')
 
 distogram = create_histogram()
 
 def generate_text(histogram, word_count=10):
+    '''
+        Generate text to be sent to the user
+    '''
     output_list = sample.weighted_sample(histogram, word_count)
     return " ".join(output_list)
 
@@ -18,4 +29,6 @@ def generate_text(histogram, word_count=10):
 def hello_world():
     return generate_text(distogram)
 
-
+if __name__ == '__main__':
+    port = os.getenv('PORT', 5000)
+    app.run(port=port)
