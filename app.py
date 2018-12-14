f23f5f939145485809480a387eb2306fcbfd5c61
C3NZ <cenz@cenz.io>
2018-12-14 04:34:32 -0800

    [feat] load markov model up from file instead of creating it every server run

--- app.py
+++ app.py
@@ -4,32 +4,43 @@
 
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 from lib.histograms.distogram import histogram
 from lib.histograms import sample
+from lib.markov import Markov
+
+from parse_jokes import get_word_queues
 
 app = Flask(__name__, template_folder='templates')
 
-def create_histogram():
-    '''
-    Create the histogram
-    '''
-    return histogram('moby.txt')
-
-# distogram = create_histogram()
-
-def generate_text(histogram, word_count=10):
-    '''
-        Generate text to be sent to the user
-    '''
-    output_list = sample.weighted_sample(histogram, word_count)
-    return " ".join(output_list)
+# All code for generating a markov chain.
+# Will keep commented for now unless I have to create a new markov for loading
+def create_markovs():
+    app.word_queues = get_word_queues(15000)
+    app.joke_markov = Markov(app.word_queues[0], order=4)
+    app.punchline_markov = Markov(app.word_queues[1], order=4)
+    app.joke_markov.save_markov('res/joke_markov')
+    app.punchline_markov.save_markov('res/punchline_markov')
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', data='hi')
+    return render_template('index.html')
+
+@app.route('/joke')
+def tell_joke():
+    joke = app.joke_markov.generate_sentence(sentence_length=20)
+    punchline = app.punchline_markov.generate_sentence(sentence_length=30)
+    joke = "Okay... " + " ".join(joke)
+    punchline = " ".join(punchline)
+    return jsonify(joke=joke, punchline=punchline)
+
 
 if __name__ == '__main__':
-    port = os.getenv('PORT', 5000)
-    app.run(port=port)
+    create_markovs()
+else:
+    # Load markov from a file to save load up time
+    app.joke_markov = Markov.load_markov('res/joke_markov')
+    app.punchline_markov = Markov.load_markov('res/punchline_markov')
+
+
